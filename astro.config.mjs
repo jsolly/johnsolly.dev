d5ea4bf097e54f5fe38c22dc9a93de5bbd374ee3
John Solly <jsolly@protonmail.com>
2024-02-17 15:44:16 -0500

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://johnsolly.dev",
-  integrations: [tailwind()]
 });
