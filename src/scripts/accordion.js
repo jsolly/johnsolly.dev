17617904848c9e7dd4c18a12d578967e46b0be82
John Solly <jsolly@protonmail.com>
2024-03-01 22:39:00 -0500

    .

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -2,19 +2,19 @@ document.addEventListener("DOMContentLoaded", initAccordion);
 
 function initAccordion() {
 	const accordionHeaders = document.querySelectorAll(".accordion-header");
-	accordionHeaders.forEach((header) => {
+	for (const header of accordionHeaders) {
 		header.addEventListener("click", () => toggleAccordion(header));
-	});
+	}
 }
 
 function toggleAccordion(accordionHeader) {
 	const accordionItem = accordionHeader.parentElement;
 	const accordionCollapse = accordionItem.querySelector(".accordion-collapse");
-	const accordionButton = accordionHeader.querySelector(".accordion-button");
+	const accordionBtn = accordionHeader.querySelector(".accordion-btn");
 	const isExpanded = accordionCollapse.classList.toggle("expanded");
 
 	// Update aria-expanded attribute
-	accordionButton.setAttribute("aria-expanded", isExpanded);
+	accordionBtn.setAttribute("aria-expanded", isExpanded);
 
 	// Set the height for the accordion collapse
 	accordionCollapse.style.height = isExpanded
