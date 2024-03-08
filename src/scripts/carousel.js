804308b257c7b18e79bd854b167a114ae547b996
John Solly <jsolly@protonmail.com>
2024-03-08 12:30:48 -0500

    bugfix: Active indicator updates with carousel

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -1,29 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
 	let currentCarouselIndex = 0;
+	let previousActiveIndex = 0;
 	const carouselItems = document.querySelectorAll(".carousel-item");
 	const indicators = document.querySelectorAll("[data-carousel-index]");
 
 	// Function to update the visibility of carousel items and the active indicator
 	const updateCarousel = () => {
-		carouselItems.forEach((item, index) => {
-			if (index === currentCarouselIndex) {
-				item.classList.add("block");
-				item.classList.remove("hidden");
-			} else {
-				item.classList.add("hidden");
-				item.classList.remove("block");
-			}
-		});
+		if (previousActiveIndex !== currentCarouselIndex) {
+			// Hide the previously visible item, if it exists
+			carouselItems[previousActiveIndex].classList.add("hidden");
+			carouselItems[previousActiveIndex].classList.remove("block");
 
-		indicators.forEach((indicator, index) => {
-			if (index === currentCarouselIndex) {
-				indicator.classList.add("bg-accent");
-				indicator.classList.remove("bg-gray-300");
-			} else {
-				indicator.classList.add("bg-gray-300");
-				indicator.classList.remove("bg-accent");
-			}
-		});
+			// Remove the background color from the previously active indicator
+			indicators[previousActiveIndex].classList.remove("bg-accent");
+			indicators[previousActiveIndex].classList.add("bg-gray-300");
+		}
+
+		// Show the current item
+		carouselItems[currentCarouselIndex].classList.add("block");
+		carouselItems[currentCarouselIndex].classList.remove("hidden");
+
+		// Add the background color to the active indicator
+		indicators[currentCarouselIndex].classList.remove("bg-gray-300");
+		indicators[currentCarouselIndex].classList.add("bg-accent");
+
+		// Update the previously visible item index
+		previousActiveIndex = currentCarouselIndex;
 	};
 
 	// Function to move carousel to the next or previous item
@@ -51,6 +53,5 @@ document.addEventListener("DOMContentLoaded", () => {
 		});
 	}
 
-	// Initialize the carousel display
 	updateCarousel();
 });
