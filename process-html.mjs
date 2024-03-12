85bdbff2ef7563675c3e8364230554e13a4c5d04
John Solly <jsolly@protonmail.com>
2024-03-11 21:38:26 -0400

    post-process html

---
+++ process-html.mjs
@@ -0,0 +1,38 @@
+import fs from "node:fs/promises";
+import { globby } from "globby";
+import { minify } from "html-minifier";
+import { JSDOM } from "jsdom";
+
+// Get all HTML files from the output directory
+const path = "./dist";
+const files = await globby(`${path}/**/*.html`);
+
+await Promise.all(
+	files.map(async (file) => {
+		console.log("Processing file:", file);
+		let html = await fs.readFile(file, "utf-8");
+
+		// Add IDs to h2, h3, and h4 tags
+		const dom = new JSDOM(html);
+		const headings = dom.window.document.querySelectorAll("h2, h3, h4");
+		for (let i = 0; i < headings.length; i++) {
+			const heading = headings[i];
+			const text = heading.textContent;
+			const id = text
+				.trim()
+				.replace(/[\s.,?:]+/g, "-")
+				.replace(/-+$/, "")
+				.toLowerCase();
+			heading.setAttribute("id", id);
+		}
+		html = dom.serialize();
+
+		// Minify the HTML
+		html = minify(html, {
+			removeComments: true,
+			preserveLineBreaks: true,
+			collapseWhitespace: true,
+		});
+		await fs.writeFile(file, html);
+	}),
+);
