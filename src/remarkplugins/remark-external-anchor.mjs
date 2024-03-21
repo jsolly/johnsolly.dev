fdf528b7823448cfe79ba582a0f188516d36cc44
John Solly <jsolly@protonmail.com>
2024-03-21 18:56:09 -0400

    safely parse urls

--- src/remarkplugins/remark-external-anchor.mjs
+++ src/remarkplugins/remark-external-anchor.mjs
@@ -1,14 +1,25 @@
 import { visit } from "unist-util-visit";
 
-const site = "https://johnsolly.dev";
+// Locally, I put localhost:4321 in the allowed hosts list
+// inside a .env.local file
+const relativeDomains = import.meta.env.VITE_ALLOWED_HOSTS.split(",") || [
+	"johnsolly.dev",
+	"www.johnsolly.dev",
+];
 
 export function remarkExternalAnchor() {
 	return (tree, _) => {
 		visit(tree, "link", (node) => {
-			if (
-				/^(https?):\/\/[^\s/$.?#].[^\s]*$/i.test(node.url) &&
-				!node.url.includes(site)
-			) {
+			let host;
+			try {
+				host = new URL(node.url).host;
+			} catch (_) {
+				// URL might be invalid or not fully qualified, e.g., a relative URL
+				host = null;
+			}
+
+			// In development, localhost is allowed; otherwise, check against allowedHosts
+			if (host && !relativeDomains.includes(host)) {
 				node.data ??= {};
 				node.data.hProperties ??= {};
 				node.data.hProperties.target = "_blank";
