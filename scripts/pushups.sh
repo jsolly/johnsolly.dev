cb01ca09aa07f4a73dd1595b460e8cfa5fc9bd9f
John Solly <9572232+jsolly@users.noreply.github.com>
2024-09-10 10:08:56 -0400

    wip

--- scripts/pushups.sh
+++ scripts/pushups.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
-# Force the script to be interactive
 exec < /dev/tty
 
-# Ask the user to do 10 pushups before continuing
 echo "Before committing, please do 10 pushups! Press Enter when you're done:"
 
 # Wait for the user to press Enter
