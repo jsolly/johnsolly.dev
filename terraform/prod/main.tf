4e378978f8ce2d24b053fb8e1529dcf31c3555bf
John Solly <9572232+jsolly@users.noreply.github.com>
2024-12-02 18:51:58 -0500

    wip

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "jsolly-general-tf-state"
-    key            = "prod/fix-my-resume/terraform.tfstate"
+    key            = "prod/hackmycareer/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
 
@@ -22,25 +22,37 @@ provider "aws" {
   profile = "prod-admin"
 }
 
-module "route53" {
-  source                    = "../modules/route53"
-  cloudfront_domain_name    = module.cloudfront.domain_name
-  cloudfront_hosted_zone_id = module.cloudfront.hosted_zone_id
-}
-
-module "acm" {
-  source  = "../modules/acm"
-  zone_id = module.route53.zone_id
-}
-
-module "cloudfront" {
-  source             = "../modules/cloudfront"
-  bucket_domain_name = module.s3.bucket_domain_name
-  bucket_id          = module.s3.bucket_id
-  certificate_arn    = module.acm.certificate_arn
+provider "aws" {
+  alias   = "us_east_1"
+  region  = "us-east-1"
+  profile = "prod-admin"
 }
 
 module "s3" {
   source           = "../modules/s3"
   distribution_arn = module.cloudfront.distribution_arn
+}
+
+module "cloudfront" {
+  source                     = "../modules/cloudfront"
+  bucket_domain_name         = module.s3.bucket_domain_name
+  bucket_id                  = module.s3.bucket_id
+  certificate_arn            = module.acm.certificate_arn
+  certificate_validation_arn = module.acm.certificate_validation_arn
+}
+
+module "acm" {
+  source = "../modules/acm"
+  providers = {
+    aws.us_east_1 = aws.us_east_1
+  }
+  domain_name               = "hackmycareer.lol"
+  subject_alternative_names = ["www.hackmycareer.lol"]
+  zone_id                   = module.route53.zone_id
+}
+
+module "route53" {
+  source                    = "../modules/route53"
+  cloudfront_domain_name    = module.cloudfront.domain_name
+  cloudfront_hosted_zone_id = module.cloudfront.hosted_zone_id
 }
