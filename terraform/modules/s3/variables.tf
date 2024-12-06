dcca49beab67891ffc74ba2d4689fb238ced14c0
John Solly <9572232+jsolly@users.noreply.github.com>
2024-12-06 09:14:37 -0500

    remove terraform (move to other repo)

--- terraform/modules/s3/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket to create"
-  type        = string
-}
-
-variable "cloudfront_distribution_arn" {
-  description = "The ARN of the CloudFront distribution"
-  type        = string
-}
